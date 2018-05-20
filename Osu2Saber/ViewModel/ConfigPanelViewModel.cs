5d83ced30f60ea592c18420f331e998474b8a756
tmokmss <tomookam@live.jp>
2018-05-20 18:17:29 +0900

    add config panel for setting parameters

---
+++ Osu2Saber/ViewModel/ConfigPanelViewModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Prism.Mvvm;
+using Osu2Saber.Model;
+
+namespace Osu2Saber.ViewModel
+{
+    class ConfigPanelViewModel : BindableBase
+    {
+        public double MaximumDifficulty
+        {
+            set
+            {
+                Osu2BsConverter.MaximumDifficulty = value;
+                RaisePropertyChanged();
+            }
+            get => Osu2BsConverter.MaximumDifficulty;
+        }
+
+        public double MinimumDifficulty
+        {
+            set
+            {
+                Osu2BsConverter.MinimumDifficulty = value;
+                RaisePropertyChanged();
+            }
+            get => Osu2BsConverter.MinimumDifficulty;
+        }
+        
+        public bool PreferHarder
+        {
+            set { Osu2BsConverter.PreferHarder = value; }
+            get => Osu2BsConverter.PreferHarder;
+        }
+    }
+}
