023bb8aa0d9ecf9cf45e9a36dab39ef748f4ca6b
tmokmss <tomookam@live.jp>
2018-05-20 18:19:42 +0900

    add error handling while converting mp3 file

--- Osu2Saber/Model/Mp3toOggConverter.cs
+++ Osu2Saber/Model/Mp3toOggConverter.cs
@@ -11,98 +11,105 @@ namespace Osu2Saber.Model
         public static string ConvertMp3toOgg(string mp3Path, string outputDir)
         {
             var oggName = Path.GetFileNameWithoutExtension(mp3Path) + ".ogg";
-
-            using (var mp3 = new Mp3FileReader(mp3Path))
-            using (var wav = WaveFormatConversionStream.CreatePcmStream(mp3))
-            using (var ogg = new FileStream(Path.Combine(outputDir, oggName), FileMode.Create, FileAccess.Write))
+            try
             {
-                const int SampleSize = 4096;
-
-                // todo: if !isValid, we cannot use ogg converter
-                var isValid = wav.WaveFormat.SampleRate == 44100;
-                if (!isValid) return ConvertMp3toWav(mp3Path, outputDir);
-
-                // Here's a reference for the code below: 
-                //   https://github.com/SteveLillis/.NET-Ogg-Vorbis-Encoder/blob/master/OggVorbisEncoder.Example/Encoder.cs
-                var info = VorbisInfo.InitVariableBitRate(2, 44100, 0.1f);
-
-                // set up our packet->stream encoder
-                var serial = new Random().Next();
-                var oggStream = new OggStream(serial);
-
-                // =========================================================
-                // HEADER
-                // =========================================================
-                // Vorbis streams begin with three headers; the initial header (with
-                // most of the codec setup parameters) which is mandated by the Ogg
-                // bitstream spec.  The second header holds any comment fields.  The
-                // third header holds the bitstream codebook.
-                var headerBuilder = new HeaderPacketBuilder();
-
-                var comments = new Comments();
-                // comments.AddTag("ARTIST", "TEST");
-
-                var infoPacket = headerBuilder.BuildInfoPacket(info);
-                var commentsPacket = headerBuilder.BuildCommentsPacket(comments);
-                var booksPacket = headerBuilder.BuildBooksPacket(info);
-
-                oggStream.PacketIn(infoPacket);
-                oggStream.PacketIn(commentsPacket);
-                oggStream.PacketIn(booksPacket);
-
-                // Flush to force audio data onto its own page per the spec
-                OggPage page;
-                while (oggStream.PageOut(out page, true))
+                using (var mp3 = new Mp3FileReader(mp3Path))
+                using (var wav = WaveFormatConversionStream.CreatePcmStream(mp3))
+                using (var ogg = new FileStream(Path.Combine(outputDir, oggName), FileMode.Create, FileAccess.Write))
                 {
-                    ogg.Write(page.Header, 0, page.Header.Length);
-                    ogg.Write(page.Body, 0, page.Body.Length);
-                }
+                    const int SampleSize = 4096;
 
-                // =========================================================
-                // BODY (Audio Data)
-                // =========================================================
-                var processingState = ProcessingState.Create(info);
+                    // todo: if !isValid, we cannot use ogg converter
+                    var isValid = wav.WaveFormat.SampleRate == 44100;
+                    if (!isValid) return ConvertMp3toWav(mp3Path, outputDir);
 
-                var buffer = new float[info.Channels][];
-                buffer[0] = new float[SampleSize];
-                buffer[1] = new float[SampleSize];
+                    // Here's a reference for the code below: 
+                    //   https://github.com/SteveLillis/.NET-Ogg-Vorbis-Encoder/blob/master/OggVorbisEncoder.Example/Encoder.cs
+                    var info = VorbisInfo.InitVariableBitRate(2, 44100, 0.1f);
 
-                var readbuffer = new byte[SampleSize * 4];
-                while (!oggStream.Finished)
-                {
-                    var bytes = wav.Read(readbuffer, 0, readbuffer.Length);
+                    // set up our packet->stream encoder
+                    var serial = new Random().Next();
+                    var oggStream = new OggStream(serial);
 
-                    if (bytes == 0)
+                    // =========================================================
+                    // HEADER
+                    // =========================================================
+                    // Vorbis streams begin with three headers; the initial header (with
+                    // most of the codec setup parameters) which is mandated by the Ogg
+                    // bitstream spec.  The second header holds any comment fields.  The
+                    // third header holds the bitstream codebook.
+                    var headerBuilder = new HeaderPacketBuilder();
+
+                    var comments = new Comments();
+                    // comments.AddTag("ARTIST", "TEST");
+
+                    var infoPacket = headerBuilder.BuildInfoPacket(info);
+                    var commentsPacket = headerBuilder.BuildCommentsPacket(comments);
+                    var booksPacket = headerBuilder.BuildBooksPacket(info);
+
+                    oggStream.PacketIn(infoPacket);
+                    oggStream.PacketIn(commentsPacket);
+                    oggStream.PacketIn(booksPacket);
+
+                    // Flush to force audio data onto its own page per the spec
+                    OggPage page;
+                    while (oggStream.PageOut(out page, true))
                     {
-                        processingState.WriteEndOfStream();
+                        ogg.Write(page.Header, 0, page.Header.Length);
+                        ogg.Write(page.Body, 0, page.Body.Length);
                     }
-                    else
-                    {
-                        var samples = bytes / 4;
 
-                        for (var i = 0; i < samples; i++)
+                    // =========================================================
+                    // BODY (Audio Data)
+                    // =========================================================
+                    var processingState = ProcessingState.Create(info);
+
+                    var buffer = new float[info.Channels][];
+                    buffer[0] = new float[SampleSize];
+                    buffer[1] = new float[SampleSize];
+
+                    var readbuffer = new byte[SampleSize * 4];
+                    while (!oggStream.Finished)
+                    {
+                        var bytes = wav.Read(readbuffer, 0, readbuffer.Length);
+
+                        if (bytes == 0)
                         {
-                            // uninterleave samples
-                            buffer[0][i] = (short)((readbuffer[i * 4 + 1] << 8) | (0x00ff & readbuffer[i * 4])) / 32768f;
-                            buffer[1][i] = (short)((readbuffer[i * 4 + 3] << 8) | (0x00ff & readbuffer[i * 4 + 2])) / 32768f;
+                            processingState.WriteEndOfStream();
                         }
+                        else
+                        {
+                            var samples = bytes / 4;
 
-                        processingState.WriteData(buffer, samples);
-                    }
+                            for (var i = 0; i < samples; i++)
+                            {
+                                // uninterleave samples
+                                buffer[0][i] = (short)((readbuffer[i * 4 + 1] << 8) | (0x00ff & readbuffer[i * 4])) / 32768f;
+                                buffer[1][i] = (short)((readbuffer[i * 4 + 3] << 8) | (0x00ff & readbuffer[i * 4 + 2])) / 32768f;
+                            }
 
-                    while (!oggStream.Finished
-                           && processingState.PacketOut(out OggPacket packet))
-                    {
-                        oggStream.PacketIn(packet);
+                            processingState.WriteData(buffer, samples);
+                        }
 
                         while (!oggStream.Finished
-                               && oggStream.PageOut(out page, false))
+                               && processingState.PacketOut(out OggPacket packet))
                         {
-                            ogg.Write(page.Header, 0, page.Header.Length);
-                            ogg.Write(page.Body, 0, page.Body.Length);
+                            oggStream.PacketIn(packet);
+
+                            while (!oggStream.Finished
+                                   && oggStream.PageOut(out page, false))
+                            {
+                                ogg.Write(page.Header, 0, page.Header.Length);
+                                ogg.Write(page.Body, 0, page.Body.Length);
+                            }
                         }
                     }
                 }
+            } catch (NAudio.MmException e)
+            {
+                Console.WriteLine(e);
+                // Just retry
+                return ConvertMp3toOgg(mp3Path, outputDir);
             }
             return oggName;
         }
@@ -111,10 +118,17 @@ namespace Osu2Saber.Model
         {
             var wavName = Path.GetFileNameWithoutExtension(mp3Path) + ".wav";
 
-            using (var mp3 = new Mp3FileReader(mp3Path))
-            using (var wav = WaveFormatConversionStream.CreatePcmStream(mp3))
+            try
             {
-                WaveFileWriter.CreateWaveFile(Path.Combine(outputDir, wavName), wav);
+                using (var mp3 = new Mp3FileReader(mp3Path))
+                using (var wav = WaveFormatConversionStream.CreatePcmStream(mp3))
+                {
+                    WaveFileWriter.CreateWaveFile(Path.Combine(outputDir, wavName), wav);
+                }
+            } catch (NAudio.MmException e)
+            {
+                Console.WriteLine(e);
+                return ConvertMp3toWav(mp3Path, outputDir);
             }
             return wavName;
         }
