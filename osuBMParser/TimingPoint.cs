510fae1832f742a6a055761d6d38b9c8fed24fe0
tmokmss <tomookam@live.jp>
2018-05-21 21:33:25 +0900

    remove unnecessary usings and references

--- osuBMParser/TimingPoint.cs
+++ osuBMParser/TimingPoint.cs
@@ -1,11 +1,4 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace osuBMParser
+﻿namespace osuBMParser
 {
     public class TimingPoint
     {
