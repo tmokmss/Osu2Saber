31e3e8c89c6cac0f9276a397ffe042d09ab59dc6
tmokmss <tomookam@live.jp>
2018-05-24 02:23:39 +0900

    improve the converting algorithm
    now automatically removes excess notes

--- Osu2Saber/Model/Algorithm/ConvertAlgorithm.cs
+++ Osu2Saber/Model/Algorithm/ConvertAlgorithm.cs
@@ -8,10 +8,14 @@ namespace Osu2Saber.Model.Algorithm
 {
     class ConvertAlgorithm
     {
-        protected const double OsuScreenXMax = 512, OsuScreenYMax = 384;
-        const int NegligibleTimeDiffMs = 500;
-        const int EnoughIntervalMs = 1500;
-        const int EnoughIntervalForSymMs = 800;
+        public static bool HandleHitSlider = false;
+
+        protected const float OsuScreenXMax = 512, OsuScreenYMax = 384;
+        
+        const int NegligibleTimeDiffMs = 500; // is used for the first note and event time gap
+        const int EnoughIntervalMs = 1500;  // is used to determine whether to reset cut direction
+        const int EnoughIntervalForSymMs = 800; // is used to determine to set a symmetric note
+        const float EnoughIntervalBetweenNotes = 0.25f;
 
         protected Beatmap org;
         protected SaberBeatmap dst;
@@ -41,6 +45,7 @@ namespace Osu2Saber.Model.Algorithm
         public void Convert()
         {
             MakeHitObjects();
+            RemoveExcessNotes();
             SetCutDirection();
             AddSymmetryNotes();
             MakeLightEffect();
@@ -51,10 +56,113 @@ namespace Osu2Saber.Model.Algorithm
             foreach (var obj in org.HitObjects)
             {
                 if (obj.Time < NegligibleTimeDiffMs) continue;
-                var (line, layer) = DeterminePosition(obj.Position.x, obj.Position.y);
-                var note = new Note(ConvertTime(obj.Time), line, layer, DetermineColor(line, layer), CutDirection.Any);
-                notes.Add(note);
+                if (obj is HitSlider && HandleHitSlider)
+                {
+                    var temp = (HitSlider)obj;
+
+                    var beatDuration = GetBeatDurationIn(temp.Time);
+                    var sliderMultiplier = org.SliderMultiplier;
+                    var durationMs = (int)Math.Round(temp.PixelLength / (100.0 * sliderMultiplier) * beatDuration);
+                    var lastPos = temp.HitSliderSegments[temp.HitSliderSegments.Count - 1].position;
+
+                    for (var i = 0; i < temp.Repeat; i++)
+                    {
+                        if (i == 0)
+                        {
+                            // add beginning and final point
+                            AddNote(temp.Time, temp.Position.x, obj.Position.y);
+                            AddNote(temp.Time + durationMs, lastPos.x, lastPos.y);
+                        }
+                        else if (i % 2 == 0)
+                        {
+                            AddNote(temp.Time + durationMs * (i + 1), temp.Position.x, obj.Position.y);
+                        }
+                        else
+                        {
+                            AddNote(temp.Time + durationMs * (i + 1), lastPos.x, lastPos.y);
+                        }
+                    }
+                }
+                else if (obj is HitSpinner)
+                {
+                    var temp = (HitSpinner)obj;
+                    AddNote(temp.Time, temp.Position.x, temp.Position.y);
+                    
+                    // this is just a nuisance at many times
+                    // AddNote(temp.EndTime, temp.Position.x, temp.Position.y);
+                }
+                else
+                {
+                    AddNote(obj.Time, obj.Position.x, obj.Position.y);
+                }
+
             }
+        }
+
+        void RemoveExcessNotes()
+        {
+            var rightNotes = Notes.Where(note => note._type == (int)(NoteType.Blue)).ToList();
+            var leftNotes = Notes.Where(note => note._type == (int)(NoteType.Red)).ToList();
+            var newRight = RemoveExcessNotes(rightNotes);
+            var newLeft = RemoveExcessNotes(leftNotes);
+            foreach (var note in newLeft)
+            {
+                newRight.Add(note);
+            }
+            notes = newRight.OrderBy(note => note._time).ToList();
+        }
+
+        List<Note> RemoveExcessNotes(List<Note> notes)
+        {
+            float lastX = 0, lastY = 0;
+            double lastTime = -100;
+            var newNotes = new List<Note>();
+            foreach (var note in notes)
+            {
+                var distance = Math.Sqrt(Math.Pow(lastX - note._lineIndex, 2) * 9.0 / 16 + Math.Pow(lastY - note._lineLayer, 2));
+                var timeGap = note._time - lastTime;
+
+                if (timeGap > EnoughIntervalBetweenNotes * (1 + 0.4 * distance))
+                {
+                    // the timeGap is long enough
+                    newNotes.Add(note);
+
+                    lastTime = note._time;
+                    lastX = note._lineIndex;
+                    lastY = note._lineLayer;
+                }
+            }
+            return newNotes;
+        }
+
+        void AddNote(int timeMs, float posx, float posy)
+        {
+            var (line, layer) = DeterminePosition(posx, posy);
+            var note = new Note(ConvertTime(timeMs), line, layer, DetermineColor(line, layer), CutDirection.Any);
+            notes.Add(note);
+        }
+
+        double GetBeatDurationIn(int timeMs)
+        {
+            var baseMsPerBeat = org.TimingPoints[0].MsPerBeat;
+            var fac = 1.0;
+            foreach (var point in org.TimingPoints)
+            {
+                if (point.MsPerBeat > 0)
+                {
+                    baseMsPerBeat = point.MsPerBeat;
+                    fac = 1;
+                }
+                else
+                    fac = -point.MsPerBeat / 100.0;
+
+                // TimingPoints is supposed to be sorted by default
+                if (point.Offset >= timeMs)
+                {
+                    break;
+                }
+            }
+            return baseMsPerBeat * fac;
         }
 
         protected double ConvertTime(int timeMs)
@@ -71,6 +179,12 @@ namespace Osu2Saber.Model.Algorithm
 
         (int line, int layer) DeterminePosition(float x, float y)
         {
+            if (x < 0) x = 0;
+            else if (x > OsuScreenXMax) x = OsuScreenXMax;
+
+            if (y < 0) y = 0;
+            else if (y > OsuScreenYMax) y = OsuScreenYMax;
+
             // just map notes position to BS screen
             var line = (int)Math.Floor(x / (OsuScreenXMax + 1) * (double)Line.MaxNum);
             var layer = (int)Math.Floor(y / (OsuScreenYMax + 1) * (double)Layer.MaxNum);
@@ -85,7 +199,7 @@ namespace Osu2Saber.Model.Algorithm
             if (line == (int)Line.Left || line == (int)Line.Right) return layer;
 
             // The larger this value is, the less likely notes appear in center middle.
-            var fineSection = 12;
+            var fineSection = 8;
             var layerIdx = (int)Math.Floor(y / (OsuScreenYMax + 1) * fineSection);
             if (layerIdx == fineSection / 2) return layer;
             if (layerIdx < fineSection / 2) return (int)Layer.Bottom;
@@ -101,7 +215,7 @@ namespace Osu2Saber.Model.Algorithm
         void MakeLightEffect()
         {
             var lastVolume = org.TimingPoints[0].Volume;
-            var lastObjTime = org.HitObjects[org.HitObjects.Count - 1].Time;
+            var lastObjTimeMs = org.HitObjects[org.HitObjects.Count - 1].Time;
             foreach (var bp in org.BreakPeriods)
             {
                 // turn off lights while break
@@ -126,6 +240,8 @@ namespace Osu2Saber.Model.Algorithm
                 events.Add(ev);
                 ev = new Event(endTime, EventType.LightTrackRingNeons, color + 1);
                 events.Add(ev);
+                ev = new Event(endTime, EventType.LightBottomBackSideLasers, color + 1);
+                events.Add(ev);
             }
 
             var lastTpTime = -1000000;
@@ -141,10 +257,14 @@ namespace Osu2Saber.Model.Algorithm
                 ev = new Event(time, EventType.RotationSmallTrackRings, 1);
                 events.Add(ev);
 
+                if (tp.Offset > lastObjTimeMs + EnoughIntervalMs)
+                    continue;
 
                 if (tp.KiaiMode)
                 {
-                    ev = new Event(time, EventType.LightTrackRingNeons, Inverse(color));
+                    ev = new Event(time, EventType.LightTrackRingNeons, Inverse(color) + 2);
+                    events.Add(ev);
+                    ev = new Event(time, EventType.LightBackTopLasers, color + 2);
                     events.Add(ev);
                     ev = new Event(time, EventType.LightRightLasers, color);
                     events.Add(ev);
@@ -215,27 +335,41 @@ namespace Osu2Saber.Model.Algorithm
             var n = notes.Count;
             if (n < 1) return;
 
-            //SetCutDirection(null, notes[0]);
+            // the first cut direction can be determined independently
             notes[0]._cutDirection = (int)PickBestDirectionSingle(notes[0]._lineIndex, notes[0]._lineLayer);
+
+            // the other notes should be determined depending on one before note.
             for (var i = 1; i < n; i++)
             {
-                var now = notes[i];
-                SetCutDirection(notes[i - 1], now);
+                SetCutDirection(notes[i - 1], notes[i]);
             }
         }
 
         void SetCutDirection(Note before, Note now)
         {
-            var swingFac = Math.Pow(ConvertBeat(now._time - before._time) * 1.0 / EnoughIntervalMs, 1.0 / 5);
+            var swingFac = Math.Pow(ConvertBeat(now._time - before._time) * 1.0 / EnoughIntervalMs, 1.0 / 2);
             if (swingFac > 1)
             {
-                //now._cutDirection = (int)PickBestDirectionSingle(now._lineIndex, now._lineLayer);
-                now._cutDirection = (int)PickBestDirectionCont(now, before, swingFac);
+                now._cutDirection = (int)PickBestDirectionSingle(now._lineIndex, now._lineLayer);
+                //now._cutDirection = (int)PickBestDirectionCont(now, before, swingFac);
             }
             else
             {
                 now._cutDirection = (int)PickBestDirectionCont(now, before, swingFac);
             }
+        }
+
+        // the best cut direction for each section below
+        //  8  9 10 11
+        //  4  5  6  7
+        //  0  1  2  3 
+        int[] bestDir = new int[] { 6, 1, 1, 7, 2, 8, 8, 3, 4, 0, 0, 5 };
+        CutDirection PickBestDirectionSingle(int line, int layer)
+        {
+            var idx = (int)Line.MaxNum * layer + line;
+            var best = (CutDirection)bestDir[idx];
+            if (best == CutDirection.Any) return PickRandomDirection();
+            return best;
         }
 
         CutDirection PickRandomDirection(DirectionRandomMode mode = DirectionRandomMode.Any)
@@ -253,18 +387,10 @@ namespace Osu2Saber.Model.Algorithm
             return (CutDirection)rnd4dir.Next(min, max);
         }
 
-        int[] bestDir = new int[] { 6, 1, 1, 7, 3, 8, 8, 2, 4, 0, 0, 5 };
-        CutDirection PickBestDirectionSingle(int line, int layer)
-        {
-            var idx = (int)Line.MaxNum * layer + line;
-            var best = (CutDirection)bestDir[idx];
-            if (best == CutDirection.Any) return PickRandomDirection();
-            return best;
-        }
-
-        // positon difference after the specified direction of cut
-        int[] lineDiff = new int[] { 0, 0, 1, -1, -1, 1, -1, 1 };
-        int[] layerDiff = new int[] { 1, -1, 0, 0, 1, 1, -1, -1 };
+        // positon difference in each axis after the specified direction of cut
+        static float Sqr2 = (float)Math.Sqrt(2);
+        float[] lineDiff = new float[] { 0, 0, -1, 1, -Sqr2, Sqr2, -Sqr2, Sqr2 };
+        float[] layerDiff = new float[] { 1, -1, 0, 0, Sqr2, Sqr2, -Sqr2, -Sqr2 };
 
         CutDirection PickBestDirectionCont(Note now, Note before, double swingAmount)
         {
@@ -273,7 +399,7 @@ namespace Osu2Saber.Model.Algorithm
                 lastcut = (int)PickRandomDirection();
 
             // limit factor
-            swingAmount = Math.Max(swingAmount, 1.5);
+            swingAmount = Math.Max(swingAmount, 2.5);
 
             // this is where player's hand supposed to be
             var nowline = before._lineIndex + lineDiff[lastcut] * swingAmount;
@@ -287,6 +413,7 @@ namespace Osu2Saber.Model.Algorithm
 
         CutDirection PickDirectionFromDeg(double deg)
         {
+            // divide a circle into 8 sections
             const double Div = 45;
             if (deg >= 180 - Div / 2) return CutDirection.Left;
             if (deg >= 180 - Div * 3 / 2) return CutDirection.UpLeft;
